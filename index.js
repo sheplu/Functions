88b43289c3a9d11d3535c2528d05ee77cc6d1f32
sheplu <jean.burellier@gmail.com>
2017-06-20 14:47:52 +0200

    update html, create findAndReplace and findAndReplaceAll function

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 'use strict';
 
 var jsonUpdate = {
-  'selector' : '',
-  'value' : ``
+  'selector' : `div ul li`,
+  'value' : `<li>new liste 1</li>`
+}
+
+var jsonUpdateAll = {
+  'selector' : `li`,
+  'value' : `<li>new liste 1</li>`
 }
 
 function findAndReplace(jsonObject) {
-
+  var elem = document.querySelector(jsonObject.selector);
+  elem.innerHTML = jsonObject.value;
 }
+
+function findAndReplaceAll(jsonObject) {
+  var elem = document.querySelectorAll(jsonObject.selector);
+  for (var i = 0; i < elem.length; i++) {
+    elem[i].innerHTML = jsonObject.value;
+  }
+}
+
+findAndReplace(jsonUpdate);
+findAndReplaceAll(jsonUpdateAll);
